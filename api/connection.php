0c4649bf182c2fc2034019c417ffca9f211e6ff8
Gustavo Shigueo <gustavo.gsmn@gmail.com>
2021-05-19 03:58:13 +0000

    Updating config

--- api/connection.php
+++ api/connection.php
@@ -7,7 +7,7 @@
   $con = mysqli_connect($host, $user, $password, $database);
   if ($con -> connect_error) echo "Fail " . $con -> connect_error;
 
-  header("Access-Control-Allow-Origin: https://5500-purple-sailfish-i4x322q5.ws-us04.gitpod.io");
+  header("Access-Control-Allow-Origin: https://8081-copper-hoverfly-xk5pqfy8.ws-us04.gitpod.io");
   header("Access-Control-Allow-Headers: *");
   header("Access-Control-Expose-Headers: *");
   header("Access-Control-Allow-Methods: *");
